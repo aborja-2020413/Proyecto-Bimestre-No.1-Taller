f3629fe1bc3080ed1d59b1e79a2542b5e0def918
aborja-2020413 <aborja-2020413@Kinal.edu.gt>
2025-03-01 21:02:48 -0600

    se agrega validacion del token

--- src/administrator/administrator.routes.js
+++ src/administrator/administrator.routes.js
@@ -1,31 +1,30 @@
 
 import {Router} from 'express'
 import {addProduct, getAllProducts, getProductById, editProduct, deleteProduct, getOutOfStockProducts, getTopSellers} from './administrator.controller.js'
-
-//import { validateJwt } from '../../middlewares/validate.jwt.js'
+import { validateJwt } from '../../middlewares/validate.jwt.js'
 
 const api = Router()
 
 // Ruta para agregar un nuevo producto
-api.post('/addproducts', addProduct);
+api.post('/addproducts', [validateJwt], addProduct);
 
 // Ruta para obtener todos los productos
-api.get('/products', getAllProducts);
+api.get('/products', [validateJwt] ,getAllProducts);
 
 // Ruta para obtener un producto por ID
-api.get('/products/:id', getProductById);
+api.get('/products/:id', [validateJwt], getProductById);
 
 // Ruta para editar un producto
-api.put('/products/:id', editProduct);
+api.put('/products/:id', [validateJwt], editProduct);
 
 // Ruta para eliminar un producto
-api.delete('/products/:id', deleteProduct);
+api.delete('/products/:id', [validateJwt], deleteProduct);
 
 // Ruta para obtener productos agotados
-api.get('/products/status/out-of-stock', getOutOfStockProducts);
+api.get('/products/status/out-of-stock', [validateJwt], getOutOfStockProducts);
 
 // Ruta para obtener productos más vendidos
-api.get('/products/status/top-sellers', getTopSellers);
+api.get('/products/status/top-sellers', [validateJwt], getTopSellers);
 
 
 export default api;
--- src/administrator/category/category.routes.js
+++ src/administrator/category/category.routes.js
@@ -2,21 +2,22 @@
 
 import {Router} from 'express'
 import { addCategory, getAllCategories, updateCategory, deleteCategory } from './category.controller.js'
+import { validateJwt } from '../../middlewares/validate.jwt.js'
 
 const api = Router()
 
 /*rutas para las categorias*/
 
 // Ruta para agregar una nueva categoría
-api.post('/addcategories', addCategory);
+api.post('/addcategories', [validateJwt], addCategory);
 
 // Ruta para obtener todas las categorías
-api.get('/allcategories', getAllCategories);
+api.get('/allcategories', [validateJwt], getAllCategories);
 
 // Ruta para editar una categoría
-api.put('/editcategories/:id', updateCategory);
+api.put('/editcategories/:id', [validateJwt], updateCategory);
 
 // Ruta para eliminar una categoría
-api.delete('/deletecategories/:id', deleteCategory);
+api.delete('/deletecategories/:id', [validateJwt], deleteCategory);
 
 export default api;
